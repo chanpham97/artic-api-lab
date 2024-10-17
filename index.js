805f98dba62a45bdf94cd05c4fa207a8468c7d90
Chan Pham <iamchanpham@gmail.com>
2024-10-17 00:21:14 +0000

    lab instructions

---
+++ index.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync')()
+// Task 1
+// Define a function that makes the API call to the request URL you defined in the worksheet (to get 3 dog artworks)
+// Make sure to console log all 3 titles 
+
+
+
+// Call the function and confirm it worked correctly
+
+
+// Task 2
+// Define another function that takes in a search query and a size limit
+// Copy the function above and modify it to make the API call with the function parameters instead
+// Note: you will likely need a for loop to print all the titles
+
+
+
+// Test this function by prompting the user to enter a query and size limit (two separate prompts)
+
+
+// Call the new function with the user inputs and confirm it worked correctly
+
